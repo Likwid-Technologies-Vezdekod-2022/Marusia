5c8d6ef763ea6c2e81e4136956925e473112ca3b
Fox-sys <borisberestov@ya.ru>
2022-06-04 10:48:55 +0300

    Обработка первого задания

--- marusia/views.py
+++ marusia/views.py
@@ -6,4 +6,20 @@ from rest_framework import status
 class MarusiaCommandsView(APIView):
     def get(self, request):
         data = request.data
-        return Response({'hz': 'hz'}, status.HTTP_200_OK)
+        response = {
+            "response": {
+                "text": "Привет вездекодерам!",
+                "tts": "Привет вездекодерам!",
+                # "buttons": [
+                #   {
+                #     "title": "Надпись на кнопке",
+                #     "payload": {},
+                #     "url": "https://example.com/"
+                #   }
+                # ],
+                "end_session": True
+            },
+            "version": "1.0",
+            'session': data['session']
+        }
+        return Response(response, status.HTTP_200_OK)
