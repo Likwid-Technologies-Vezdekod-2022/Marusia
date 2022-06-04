589e84baeaeaa9f1376d311eee526966bfda3d44
Fox-sys <borisberestov@ya.ru>
2022-06-04 14:45:26 +0300

    принты

--- marusia/views.py
+++ marusia/views.py
@@ -148,6 +148,7 @@ class MarusiaCommandsView(APIView):
                 return Response(response, status.HTTP_200_OK)
             answers = QUESTIONS[state['prev_question']]['answers']
             if 1 <= answer <= len(answers):
+                print(answers[answer])
                 if answers[answer][1]:
                     text = 'Правельный ответ!'
                     state['result_counter'] += 1
