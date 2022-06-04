f520023ccf661d1be044462fdf20d092a3c75645
Fox-sys <borisberestov@ya.ru>
2022-06-04 13:41:13 +0300

    заготовка для опросника

--- marusia/views.py
+++ marusia/views.py
@@ -4,6 +4,81 @@ from rest_framework import status
 from fuzzywuzzy import fuzz
 
 
+QUESTIONS = [
+    {
+        'question': 'Что такое http 404',
+        'answers': {
+            {'Кто я?': False},
+            {'Ошибка говорящая, что сервер не доступен': False},
+            {'Шуточная ошибка, в оригинале звучащая как You are teapot, в переводе: ты чайник': False},
+            {'Страница не найдена': True},
+        }
+    },
+    {
+        'question': 'Как в питоне разделить строку',
+        'answers': {
+            {'На ноль конечно': False},
+            {'Использовать встроенноую фукнцию split': False},
+            {'Использовать строковый метод split': True},
+            {'Использовать строковый метод div': False},
+        }
+    },
+    {
+        'question': 'Какой линукс чаще всего используется для работы в области систесмной безопасности?',
+        'answers': {
+            {'Windows 95': False},
+            {'kali': True},
+            {'ubuntu': False},
+            {'Arch': False},
+        }
+    },
+    {
+        'question': 'Открыто ли api у телеграма?',
+        'answers': {
+            {'Кто такой api?': False},
+            {'Да': True},
+            {'Нет': False},
+        }
+    },
+    {
+        'question': 'Что на js выдаст программа: \'2\'+\'2\'-\'2\'',
+        'answers': {
+            {'Исключение': False},
+            {'2': False},
+            {'20': True},
+            {'-infinity': False},
+        }
+    },
+    {
+        'question': 'Что такое XR',
+        'answers': {
+            {'Второе название для HR': False},
+            {'Дополненая реальность': True},
+            {'Нееронные сети': False},
+            {'Мобильная разработка': False},
+        }
+    },
+    {
+        'question': 'Самый популярный язык для мобильной разработки в 2007',
+        'answers': {
+            {'Python 3.10': False},
+            {'Kotlin': False},
+            {'Java': True},
+            {'Unity': False},
+        }
+    },
+    {
+        'question': 'На каком голосовом помошнике ты проходишь этот квиз?',
+        'answers': {
+            {'Джарвис': False},
+            {'Алиса': False},
+            {'Гоша Дударь': False},
+            {'Маруся': True},
+        }
+    },
+]
+
+
 class MarusiaCommandsView(APIView):
     def post(self, request):
         data = request.data
@@ -41,8 +116,17 @@ class MarusiaCommandsView(APIView):
             }
         if fuzz.ratio(data['request']['command'], 'Опросник') > 70:
             response['response'] = {
-                'text': 'indev',
-                'end_session': True,
-                'tts': 'in dev'
+                'text': QUESTIONS[0]['question'],
+                'end_session': False,
+                'tts': QUESTIONS[0]['question'],
+                'buttons': [],
             }
+            for answer in QUESTIONS[0]['answers']:
+                response['response']['buttons'].append(
+                    {
+                        'title': answer,
+                        'payload': QUESTIONS[0]['answers'],
+                        'url': 'https://marus.bolanebyla.ru/marusia/quiz'
+                    }
+                )
         return Response(response, status.HTTP_200_OK)
