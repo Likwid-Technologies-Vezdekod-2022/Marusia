cc3c320a68ac6c69976bfe22d20d3bf1b055de87
Fox-sys <borisberestov@ya.ru>
2022-06-04 12:13:31 +0300

    Помощь

--- marusia/views.py
+++ marusia/views.py
@@ -7,25 +7,35 @@ class MarusiaCommandsView(APIView):
     def get(self, request):
         data = request.data
         response = {
-                "response": {
-                    "text": "Неизвестная команда",
-                    "tts": "неизвестная команда",
-                    "end_session": True
-                },
-                "version": "1.0",
-                'session': data['session']
-            }
+            'response': {
+                'text': 'Неизвестная команда\nСписок команд:\nпамагите: Список команд\nlikwid technologies '
+                        'вездекод: Привет вездекодерам!\nОпросник: Список последовательно задаваемых вопросов\n\n '
+                        'Команды работают с любым регистром',
+                'tts': 'неизвестная команда',
+                'end_session': True
+            },
+            'version': '1.0',
+            'session': data['session']
+        }
         if data['request']['command'].lower() == 'likwid technologies вездекод':
             response['response'] = {
-                "text": "Привет вездекодерам!",
-                "tts": "Привет вездекодерам!",
-                # "buttons": [
+                'text': 'Привет вездекодерам!',
+                'tts': 'Привет вездекодерам!',
+                # 'buttons': [
                 #   {
-                #     "title": "Надпись на кнопке",
-                #     "payload": {},
-                #     "url": "https://example.com/"
+                #     'title': 'Надпись на кнопке',
+                #     'payload': {},
+                #     'url': 'https://example.com/'
                 #   }
                 # ],
-                "end_session": True
+                'end_session': True
+            }
+        if data['request']['command'] == 'памагите':
+            response['response'] = {
+                'text': 'Список команд:\nпамагите: Список команд\nlikwid technologies '
+                        'вездекод: Привет вездекодерам!\nОпросник: Список последовательно задаваемых вопросов\n\n '
+                        'Команды работают с любым регистром',
+                'end_session': True,
+                'tts': 'Список команд выведен'
             }
         return Response(response, status.HTTP_200_OK)
