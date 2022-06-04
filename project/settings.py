8a252ce1f351231443d1831487fcbc86d3f6c13a
Fox-sys <borisberestov@ya.ru>
2022-06-04 12:45:11 +0300

    дебаг

--- project/settings.py
+++ project/settings.py
@@ -17,7 +17,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 env = environ.Env(
-    DEBUG=(bool, False),
+    DEBUG=(bool, True),
     ALLOWED_HOSTS=(list[str], ['localhost', '127.0.0.1']),
 )
 
